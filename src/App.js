b89d64f8315b7acf46f060e442127eb2615d3f96
Eren-Yeag <esenbaevnasyr@gmail.com>
2021-12-13 19:18:33 +0300

    weatger-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import axios from "axios";
+import './style.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [weather, setWeather] = useState({});
+    const [cityName, setCityName] = useState('');
+
+    const getWeather = () => {
+        axios(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=c3ca235f299a5ac03a9b15b27ae3fee0`)
+    .then(({data}) => setWeather(data))
+    };
+
+    return (
+        <div className="App">
+            <div className='form'>
+                <h1 className='form__title'>Прогноз погоды</h1>
+                <input placeholder='Введите название города' className='form__input' type="text"
+                       onChange={(event) => setCityName(event.target.value)}/>
+                <button className='form__btn' type='button' onClick={() => getWeather()}>Получить погоду</button>
+
+
+                {JSON.stringify(weather) === '{}' ?
+                    <p className="p">Здесь будет ваша погода</p> :
+                    <div className='weather'>
+                        <div className='weather__top'>
+                            <h2>{weather.name}</h2>
+                            <h2>{weather.sys.country}</h2>
+                        </div>
+                        <div>
+                            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="logo"/>
+                            <span>{Math.trunc(weather.main.temp - 273.15)}</span>
+                        </div>
+                        <span>
+                            C
+                        </span>
+                        |
+                        <span>
+                            F
+                        </span>
+                        <p>{weather.weather[0].description}</p>
+
+                    </div>
+
+                }
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
